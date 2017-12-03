d13d9fab4cdf6d7a5b506773a4d7469e8adc7594
Max Lapan <max.lapan@gmail.com>
2017-12-03 10:58:50 +0100

    Draw quantiles

--- agents/qr_dqn_agent.py
+++ agents/qr_dqn_agent.py
@@ -16,6 +16,13 @@
 
 from agents.value_optimization_agent import *
 
+import os
+import matplotlib as mpl
+mpl.use("Agg")
+import matplotlib.pylab as plt
+
+import numpy as np
+
 
 # Quantile Regression Deep Q Network - https://arxiv.org/pdf/1710.10044v1.pdf
 class QuantileRegressionDQNAgent(ValueOptimizationAgent):
@@ -23,25 +30,68 @@ class QuantileRegressionDQNAgent(ValueOptimizationAgent):
         ValueOptimizationAgent.__init__(self, env, tuning_parameters, replicated_device, thread_id)
         self.quantile_probabilities = np.ones(self.tp.agent.atoms) / float(self.tp.agent.atoms)
         self.done_state = None
+        self.done_act = None
         self.normal_states = []
+        self.normal_acts = []
 
     # prediction's format is (batch,actions,atoms)
     def get_q_values(self, quantile_values):
         return np.dot(quantile_values, self.quantile_probabilities)
 
-    def _gather_states(self, states, overs, normal_count=5):
+    def _gather_states(self, states, actions, overs, normal_count=5):
         if self.done_state is not None and len(self.normal_states) == normal_count:
             return
-        for s, o in zip(states, overs):
+        for s, a, o in zip(states, actions, overs):
             if o and self.done_state is None:
                 self.done_state = s
+                self.done_act = a
                 continue
             if len(self.normal_states) < normal_count:
                 self.normal_states.append(s)
+                self.normal_acts.append(a)
+
+    def _plot_one(self, x, y, f_name, sort=False):
+        plt.clf()
+        q_val = self.get_q_values(y)
+        if sort:
+            y = list(sorted(y))
+        plt.plot(x, y)
+        plt.title("Inv CDF, q_val=%.4f" % q_val)
+        plt.savefig(f_name)
+
+
+    def _plot_states(self, steps=10000, out_dir='img_qr'):
+        if self.total_steps_counter % steps != 0:
+            return
+        if self.done_state is None or len(self.normal_states) < 5:
+            return
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+        done_quantiles = self.main_network.target_network.predict(np.expand_dims(self.done_state, 0))
+        done_quantiles = done_quantiles[0, self.done_act]
+        norm_quantiles = self.main_network.target_network.predict(np.array(self.normal_states))
+        norm_quantiles = norm_quantiles[range(len(self.normal_states)), self.normal_acts]
+
+        cum_probs = np.array(range(self.tp.agent.atoms+1))/float(self.tp.agent.atoms)
+        cum_probs = cum_probs[1:]
+
+        fname = os.path.join(out_dir, "raw_done_%06d.png" % self.total_steps_counter)
+        self._plot_one(cum_probs, done_quantiles, fname, sort=False)
+        fname = os.path.join(out_dir, "sorted_done_%06d.png" % self.total_steps_counter)
+        self._plot_one(cum_probs, done_quantiles, fname, sort=True)
+
+        for idx, quant in enumerate(norm_quantiles):
+            fname = os.path.join(out_dir, "raw_%d_%06d.png" % (idx, self.total_steps_counter))
+            self._plot_one(cum_probs, quant, fname, sort=False)
+            fname = os.path.join(out_dir, "sorted_%d_%06d.png" % (idx, self.total_steps_counter))
+            self._plot_one(cum_probs, quant, fname, sort=True)
+
+        return
 
     def learn_from_batch(self, batch):
         current_states, next_states, actions, rewards, game_overs, _ = self.extract_batch(batch)
-        self._gather_states(current_states, game_overs)
+        self._gather_states(current_states, actions, game_overs)
+        self._plot_states()
 
         # get the quantiles of the next states and current states
         next_state_quantiles = self.main_network.target_network.predict(next_states)
